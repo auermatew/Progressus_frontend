9261487b82f758f5649098708bd5b414acb81970
Bartha Álmos <bartha.almos0306@gmail.com>
2024-03-07 11:12:32 +0100

    Navbar 1.0 Update Meghagytam a helyeket Logonak meg a profil infoknak, es lehet valtani a logged in és logged out allapotok kozott.

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,10 +1,12 @@
 import React, { Fragment, useEffect } from "react";
+import { FiLogOut } from "react-icons/fi";
 import "./_mainPage.scss";
 import { Hero } from "./Hero";
 
 const MainPage = () => {
     useEffect(() => {
-        const interBubble = document.querySelector<HTMLDivElement>('.interactive')!;
+        const interBubble =
+            document.querySelector<HTMLDivElement>(".interactive")!;
         let curX = 0;
         let curY = 0;
         let tgX = 0;
@@ -13,13 +15,15 @@ const MainPage = () => {
         function move() {
             curX += (tgX - curX) / 20;
             curY += (tgY - curY) / 20;
-            interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+            interBubble.style.transform = `translate(${Math.round(
+                curX
+            )}px, ${Math.round(curY)}px)`;
             requestAnimationFrame(() => {
                 move();
             });
         }
 
-        window.addEventListener('mousemove', (event) => {
+        window.addEventListener("mousemove", (event) => {
             tgX = event.clientX;
             tgY = event.clientY;
         });
@@ -29,29 +33,53 @@ const MainPage = () => {
 
     return (
         <>
-            <Hero />
-        <div className="bg">
-            <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-                <filter id="goo">
-                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
-                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
-                <feBlend in="SourceGraphic" in2="goo" />
-                </filter>
-            </defs>
-            </svg>
-            <div className="gradients-container">
-                <div className="g1"></div>
-                <div className="g2"></div>
-                <div className="g3"></div>
-                <div className="g4"></div>
-                <div className="g5"></div>
-                <div className="interactive"></div>
+            <div className="navbar">
+                {/* navbar items */}
+                <div className="logoBox">Logo</div>
+                <div className="loggedIn" /* hidden */>
+                    <div className="accountBox">Account</div>
+                    <FiLogOut className="icon" size={40} />
+                </div>
+                <div className="loggedOut" hidden>
+                    <div className="loginBox">
+                        <button className="loginBtn">Bejelentkezés</button>
+                    </div>
+                    <div className="registerBox">
+                        <button className="registerBtn">Regisztráció</button>
+                    </div>
+                </div>
             </div>
-        </div>
+            <Hero />
+            <div className="bg">
+                <svg xmlns="http://www.w3.org/2000/svg">
+                    <defs>
+                        <filter id="goo">
+                            <feGaussianBlur
+                                in="SourceGraphic"
+                                stdDeviation="10"
+                                result="blur"
+                            />
+                            <feColorMatrix
+                                in="blur"
+                                mode="matrix"
+                                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                                result="goo"
+                            />
+                            <feBlend in="SourceGraphic" in2="goo" />
+                        </filter>
+                    </defs>
+                </svg>
+                <div className="gradients-container">
+                    <div className="g1"></div>
+                    <div className="g2"></div>
+                    <div className="g3"></div>
+                    <div className="g4"></div>
+                    <div className="g5"></div>
+                    <div className="interactive"></div>
+                </div>
+            </div>
         </>
     );
 };
-
 
 export default MainPage;
