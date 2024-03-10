27d8122b2ff13cd3d6027e20172f31aa9ba6b3c7
Bartha Álmos <bartha.almos0306@gmail.com>
2024-03-10 23:16:55 +0100

    MainPage - Functions row kis animalt 3 dobozos sor update

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 import { FiLogOut } from "react-icons/fi";
 import "./_mainPage.scss";
 import { Hero } from "./Hero";
@@ -30,6 +31,15 @@ const MainPage = () => {
 
         move();
     }, []);
+
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: true });
+    const control = useAnimation();
+    useEffect(() => {
+        if (isInView) {
+            control.start("visible");
+        }
+    }, [isInView]);
 
     return (
         <>
@@ -77,6 +87,31 @@ const MainPage = () => {
                     <div className="g5"></div>
                     <div className="interactive"></div>
                 </div>
+            </div>
+            <div ref={ref} className="container1">
+                <motion.div
+                    className="cards"
+                    variants={{
+                        hidden: { opacity: 0, y: 20 },
+                        visible: { opacity: 1, y: 0 },
+                    }}
+                    initial="hidden"
+                    animate={control}
+                    transition={{ duration: 1 }}
+                >
+                    <div className="col1">
+                        <h2>Blabla</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda debitis ab, quod repellendus aliquam beatae perspiciatis nihil obcaecati quia amet itaque deserunt aspernatur, enim nulla facilis, fugiat deleniti rerum doloremque.</p>
+                    </div>
+                    <div className="col2">
+                        <h2>Blabla</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci, exercitationem. Expedita ducimus facere suscipit. Eaque recusandae est molestiae nobis atque cumque velit necessitatibus natus dolor. Itaque iste totam veniam est.</p>
+                    </div>
+                    <div className="col3">
+                        <h2>Blabla</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum facere ab excepturi, odit molestias iure dicta, odio accusamus amet minima nam, explicabo ipsam et aut delectus illo ex placeat repudiandae.</p>
+                    </div>
+                </motion.div>
             </div>
         </>
     );
