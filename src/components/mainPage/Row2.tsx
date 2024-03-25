2375518957dcbebfc84477f332aaad76ed0834b4
Bartha Álmos <bartha.almos0306@gmail.com>
2024-03-25 12:08:00 +0100

    Row2 - update

--- src/components/mainPage/Row2.tsx
+++ src/components/mainPage/Row2.tsx
@@ -5,11 +5,25 @@ const Row2 = () => {
     return (
         <div className="mainRow">
             <div className="left">
-                <div className="titleBox"></div>
-                <div className="imageBox"></div>
+                <div className="titleBox">
+                    <h1>Hogyan is működik az oldalunk?</h1>
+                </div>
+                <div className="imageBox">
+                    <img src="../../../assets/images/kep.png" />
+                </div>
             </div>
             <div className="right">
-                <div className="textBox"></div>
+                <div className="textBox">
+                    <p>
+                        Az oldal lényege, hogy tanárokat/mentorokat köt össze
+                        diákokkal és tanulni kívánó embereket.<br></br>Az
+                        oldalunkon első sorban regisztrálni lehet, ahol
+                        kiválaszthatjuk a hozzánk megfelelő feladat körünket,
+                        majd ezután egy kereső oldalon megtalálhatjuk a hozzánk
+                        legjobban illő mentort. Itt megkapjuk az elérhetőségeket
+                        és már lehet is kezdeni a fejlődést!
+                    </p>
+                </div>
             </div>
         </div>
     );
